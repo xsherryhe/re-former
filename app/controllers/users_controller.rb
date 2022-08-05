89cb9e4ac00d5060723534068b25503eec7a9a26
Sherry He <xsherryhe@gmail.com>
2022-08-04 21:01:17 -0400

    Created controller action and view using HTML and form_tag for Users#new and Users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,18 @@ class UsersController < ApplicationController
   end
 
   def create
+    # username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
